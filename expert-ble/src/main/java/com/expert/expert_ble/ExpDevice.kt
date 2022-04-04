888b042399098096235695606625eebeb2ebd86c
nattapong <ch.nattapong@hotmail.co.th>
2022-04-04 20:50:27 +0700

    not check permission

--- expert-ble/src/main/java/com/expert/expert_ble/ExpDevice.kt
+++ expert-ble/src/main/java/com/expert/expert_ble/ExpDevice.kt
@@ -53,21 +53,6 @@ open class ExpDevice :ExpBle{
                 return
             }
         }
-
-        if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.BLUETOOTH_CONNECT
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
         names.add(dv.name + " " + dv.address)
         devices.add(dv)
     }
@@ -119,20 +104,6 @@ open class ExpDevice :ExpBle{
     {
         if (setDevice(dv)) {
             try {
-                if (ActivityCompat.checkSelfPermission(
-                        context,
-                        Manifest.permission.BLUETOOTH_CONNECT
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return
-                }
                 DEVICE.connectGatt(context, true, callback)
             }catch (e:Exception)
             {
